aeb876994010b0a67cf4c76a65f6be192d65c1e6
Nguyen Hong Thanh <ng.hong.thanh@gmail.com>
2018-05-14 21:34:34 +0700

    14/05/2018 begin...

---
+++ TH.Core/TH.Core/Forms/ThMessageBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TH.Core.Forms
+{
+    public static class ThMessageBox
+    {
+        public static void Show(string message)
+        {
+            MessageBox.Show(message, "Hệ thống", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ TH.Core/TH.MiniShop/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TH.Core.Forms;
+
+namespace TH.MiniShop
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        MiniShop.Models.MiniShopEntities db = new Models.MiniShopEntities();
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            ThMessageBox.Show("hihi");
+
+        }
+    }
+}
