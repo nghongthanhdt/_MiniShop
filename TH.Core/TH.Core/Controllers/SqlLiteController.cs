7eece363dc564718b6af05f621da592926bc547e
Nguyễn Hồng Thanh <ng.hong.thanh@gmail.com>
2018-06-06 15:14:00 +0700

    06/06/2018

---
+++ TH.Core/TH.Core/Controllers/SqlLiteController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH.Core.Controllers
+{
+    public class SqlLiteController
+    {
+        public string ConnectionString = "";
+
+    }
+}
